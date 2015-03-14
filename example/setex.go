0d4b05ff9ad5a3c6ffa3c5b78ce5229743947406
Steve Spencer <sspencer@playnetwork.com>
2015-03-14 11:10:24 -0700

    SDIFF and SDIFFSTORE

--- example/setex.go
+++ example/setex.go
@@ -9,5 +9,8 @@ import (
 func main() {
 	redis := godis.NewGodisConn(":6379", "", os.Stderr)
 	fmt.Println("SADD:", redis.SAdd("myset", "one", "two", "three"))
+	fmt.Println("SADD:", redis.SAdd("myset2", "three", "four", "five"))
 	fmt.Println("SCARD:", redis.SCard("myset"))
+	fmt.Println("SDIFF:", redis.SDiff("myset", "myset2"))
+	fmt.Println("SDIFFSTORE:", redis.SDiffStore("mynewset", "myset", "myset2"))
 }
--- sets.go
+++ sets.go
@@ -15,9 +15,9 @@ func (g *Godis) SAdd(key string, members ...interface{}) int {
 		conn = g.conn
 	}
 
-	keyarg := make([]interface{}, 1)
-	keyarg[0] = key
-	args := append(keyarg, members...)
+	args := make([]interface{}, 1)
+	args[0] = key
+	args = append(args, members...)
 	reply, err := conn.Do("SADD", args...)
 
 	if retval, err := redis.Int(reply, err); err != nil {
@@ -44,6 +44,58 @@ func (g *Godis) SCard(key string) int {
 	if retval, err := redis.Int(reply, err); err != nil {
 		// handle error
 		g.log.Printf("Error SCARD %s\n", err)
+		return 0
+	} else {
+		return retval
+	}
+}
+
+// SDiff returns the members of the set resulting from the difference between
+// the first set and all the successive sets.
+func (g *Godis) SDiff(key string, keys ...interface{}) []string {
+	var conn redis.Conn
+	if g.pooled {
+		conn = g.pool.Get()
+		defer conn.Close()
+	} else {
+		conn = g.conn
+	}
+
+	args := make([]interface{}, 1)
+	args[0] = key
+	args = append(args, keys...)
+	reply, err := conn.Do("SDIFF", args...)
+
+	if retval, err := redis.Strings(reply, err); err != nil {
+		// handle error
+		g.log.Printf("Error SDIFF %s\n", err)
+		return []string{}
+	} else {
+		return retval
+	}
+}
+
+// SDiffStore stores the members of the set resulting from the difference between
+// the first set and all the successive sets into destination and returns the count
+// of members in destination.
+func (g *Godis) SDiffStore(destination, key string, keys ...interface{}) int {
+	var conn redis.Conn
+	if g.pooled {
+		conn = g.pool.Get()
+		defer conn.Close()
+	} else {
+		conn = g.conn
+	}
+
+	args := make([]interface{}, 2)
+	args[0] = destination
+	args[1] = key
+	args = append(args, keys...)
+	reply, err := conn.Do("SDIFFSTORE", args...)
+
+	if retval, err := redis.Int(reply, err); err != nil {
+		// handle error
+		g.log.Printf("Error SDIFFSTORE %s\n", err)
 		return 0
 	} else {
 		return retval
