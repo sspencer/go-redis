936ae11ffca5b0c2bf6dc1b4d2f574f88b9538ed
Steve Spencer <sspencer@playnetwork.com>
2015-03-13 23:22:03 -0700

    HEXISTS

--- example/hashex.go
+++ example/hashex.go
@@ -9,4 +9,5 @@ func main() {
 	redis := godis.NewGodis()
 	fmt.Println("HGET:", redis.HGet("track123", "title"))
 	fmt.Println("HDEL:", redis.HDel("track123", "these", "do", "not", "exist"))
+	fmt.Println("HEXISTS:", redis.HExists("track123", "artist"))
 }
--- hashes.go
+++ hashes.go
@@ -4,25 +4,6 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
-// HGet gets the value of a hash field
-func (g *Godis) HGet(key, field string) string {
-	conn := g.pool.Get()
-	defer conn.Close()
-
-	reply, err := conn.Do("HGET", key, field)
-	g.log.Printf("HGET %s %s\n", key, field)
-
-	if retval, err := redis.String(reply, err); err != nil {
-		// handle error
-		g.Error = err
-		g.log.Printf("Error HGET %s\n", err)
-		return NIL
-	} else {
-		g.Error = nil
-		return retval
-	}
-}
-
 // HDel deletes one of more hash fields.
 func (g *Godis) HDel(key string, fields ...interface{}) int {
 	conn := g.pool.Get()
@@ -39,6 +20,44 @@ func (g *Godis) HDel(key string, fields ...interface{}) int {
 		g.Error = err
 		g.log.Printf("Error HDel %s\n", err)
 		return 0
+	} else {
+		g.Error = nil
+		return retval
+	}
+}
+
+// HExists determines if a hash field exists
+func (g *Godis) HExists(key, field string) bool {
+	conn := g.pool.Get()
+	defer conn.Close()
+
+	reply, err := conn.Do("HExists", key, field)
+	g.log.Printf("HExists %s %s\n", key, field)
+
+	if retval, err := redis.Bool(reply, err); err != nil {
+		// handle error
+		g.Error = err
+		g.log.Printf("Error HExists %s\n", err)
+		return false
+	} else {
+		g.Error = nil
+		return retval
+	}
+}
+
+// HGet gets the value of a hash field
+func (g *Godis) HGet(key, field string) string {
+	conn := g.pool.Get()
+	defer conn.Close()
+
+	reply, err := conn.Do("HGET", key, field)
+	g.log.Printf("HGET %s %s\n", key, field)
+
+	if retval, err := redis.String(reply, err); err != nil {
+		// handle error
+		g.Error = err
+		g.log.Printf("Error HGET %s\n", err)
+		return NIL
 	} else {
 		g.Error = nil
 		return retval
