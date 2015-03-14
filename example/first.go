a0103a305650c9ce3b643cb915c2703b68cd65ec
Steve Spencer <sspencer@playnetwork.com>
2015-03-13 19:40:31 -0700

    Add GetBit

--- example/first.go
+++ example/first.go
@@ -21,4 +21,5 @@ func main() {
 	fmt.Println("BITOP:", redis.BitOpOr("newbits", "mybit", "myotherbit"))
 	fmt.Println("DECR:", redis.Decr("mycounter"))
 	fmt.Println("DECRBY:", redis.DecrBy("mycounter", 3))
+	fmt.Println("GETBIT:", redis.GetBit("mybit", 16))
 }
