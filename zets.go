fde9a16891035a92835e44fe815576b07a0e68d6
Steve Spencer <sspencer@playnetwork.com>
2015-03-14 19:33:57 -0700

    more sorted set commands added

--- zets.go
+++ zets.go
@@ -24,13 +24,55 @@ func (g *Godis) ZCard(key string) int64 {
 	return g.cmdInt("ZCARD", key)
 }
 
+// ZCount counts the members in a sorted set with scores within the given values.
+// Note that the values do not have to be integers, but could be strings like
+// "-inf", "+inf", "(1".  Read the Redis doc.
+func (g *Godis) ZCount(key, min, max string) int64 {
+	return g.cmdInt("ZCOUNT", key, min, max)
+}
+
+// ZIncrBy increments the score of a member in a sorted set.
+func (g *Godis) ZIncrBy(key string, increment float64, member string) float64 {
+	return g.cmdFloat("ZINCRBY", key, increment, member)
+}
+
+// Skipped for now - ZInterStore
+
+// ZLexCount counts the number of members in a sorted set between a
+// given lexicographical range.
+func (g *Godis) ZLexCount(key, min, max string) int64 {
+	return g.cmdInt("ZLEXCOUNT", key, min, max)
+}
+
 // ZRange returns a range of members in a sorted set, by index
 func (g *Godis) ZRange(key string, start, stop int) []string {
-	return g.cmdStrings("ZRANGE", start, stop)
+	return g.cmdStrings("ZRANGE", key, start, stop)
 }
 
 // ZRange returns a range of members and their scores in a sorted set, by index.
 func (g *Godis) ZRangeWithScores(key string, start, stop int) []ScoreMember {
 	retval := g.cmdStrings("ZRANGE", key, start, stop, "WITHSCORES")
 	return memberScores(retval)
+}
+
+// ZRangeByLex return a range of members ina sorted set, by lexicographical range.
+func (g *Godis) ZRangeByLex(key, min, max string) []string {
+	return g.cmdStrings("ZRANGEBYLEX", key, min, max)
+}
+
+// ZRangeByLex return a range of members ina sorted set, by lexicographical range.
+func (g *Godis) ZRangeByLexLimit(key, min, max, string, offset, count int) []string {
+	return g.cmdStrings("ZRANGEBYLEX", key, min, max, "LIMIT", offset, count)
+}
+
+// ZRevRangeByLex return a range of members ina sorted set, by lexicographical range,
+// ordered from higher to lower strings.
+func (g *Godis) ZRevRangeByLex(key, max, min string) []string {
+	return g.cmdStrings("ZREVRANGEBYLEX", key, max, min)
+}
+
+// ZRevRangeByLex return a range of members ina sorted set, by lexicographical range,
+// ordered from higher to lower strings.
+func (g *Godis) ZRevRangeByLexLimit(key, max, min, string, offset, count int) []string {
+	return g.cmdStrings("ZRANGEBYLEX", key, max, min, "LIMIT", offset, count)
 }
